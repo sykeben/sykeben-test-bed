ea9583af76834b5a84c0d0c2afe4b80bde6fc826
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-03-19 12:53:17 -0400

    Added links page.
    - Fixed navbar.
    - Added links page.
    - Changed "listening" log message.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
+
   .use(express.static(path.join(__dirname, 'public')))
 
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/links', (req, res) => res.render('pages/links'))
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on port ${ PORT }...`))

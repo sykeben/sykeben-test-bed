1cecececb8c447e156727eb18ec7d793bf92a749
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-03-20 10:40:52 -0400

    Added uptime to status page.
    - Changed index.js formatting.
    - Improved logging.
    - Added uptime to status page.
    - Added "toHHMMSS" string prototype.

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ const logger = require('heroku-logger')
 const express = require('express')
 const path = require('path')
 
-// Define and start the server.
-logger.info('Starting server...')
+// Define the server.
+logger.info('Defining server...')
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-  .use(express.static(path.join(__dirname, 'public')))
+// Define pages.
+logger.info('Loading pages...')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/links', (req, res) => res.render('pages/links'))
+app.get('/status', (req, res) => res.render('pages/status'))
 
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+// Start the server.
+app.listen(PORT, () => logger.info(`Server listening.`, { port: PORT }))
 
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/links', (req, res) => res.render('pages/links'))
-  .get('/status', (req, res) => res.render('pages/status'))
+// Define HHMMSS function for use on the status page.
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-  .listen(PORT, () => logger.info(`Server listening.`, { port: PORT }))
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  var time    = hours+':'+minutes+':'+seconds;
+  return time;
+}
